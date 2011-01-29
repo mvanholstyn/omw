98e4e9f1092ff73e0b6673b71b413a61e3677350
Mark Van Holstyn <mvanholstyn@gmail.com>
2011-01-29 10:26:20 -0500

    clean phonegap application

---
+++ Classes/lotswholetimeAppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  lotswholetimeAppDelegate.h
+//  lotswholetime
+//
+//  Created by Mark Van Holstyn on 1/26/11.
+//  Copyright Mutually Human Software 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PhoneGapDelegate.h"
+
+@interface lotswholetimeAppDelegate : PhoneGapDelegate {
+}
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  lotswholetime
+//
+//  Created by Mark Van Holstyn on 1/26/11.
+//  Copyright Mutually Human Software 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"lotswholetimeAppDelegate");
+    [pool release];
+    return retVal;
+}
